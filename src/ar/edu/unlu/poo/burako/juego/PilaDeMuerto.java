b90645c37e979236dcfef60885879b720e2d2e88
germanritacco <germanritacco@gmail.com>
2023-10-29 20:13:50 -0300

    Clase PilaDeMuerto

---
+++ src/ar/edu/unlu/poo/burako/juego/PilaDeMuerto.java
@@ -0,0 +1,11 @@
+package ar.edu.unlu.poo.burako.juego;
+
+import java.util.ArrayList;
+
+public class PilaDeMuerto extends Pila {
+
+    public PilaDeMuerto() {
+        pila = new ArrayList<>(11);
+    }
+
+}
