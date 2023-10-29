0d75fbea2e9a260a5b8f1dcfd5a84dd4325fe8e5
germanritacco <germanritacco@gmail.com>
2023-10-29 20:14:22 -0300

    Clase Dado, tirar dado

---
+++ src/ar/edu/unlu/poo/burako/juego/Dado.java
@@ -0,0 +1,17 @@
+package ar.edu.unlu.poo.burako.juego;
+
+import static java.lang.Math.random;
+
+public class Dado {
+
+    private Integer cara;
+
+    public Integer getCara() {
+        return cara;
+    }
+
+    public void tirarDado() {
+        this.cara = (int) (random() * 6) + 1; // Random entre 1 y 6
+    }
+
+}
