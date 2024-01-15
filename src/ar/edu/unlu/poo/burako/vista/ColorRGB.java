8fe1d778e248c8d6d921e69c9ae647388fa8db8f
germanritacco <germanritacco@gmail.com>
2024-01-15 19:32:05 -0300

    Implementado Clase con colores RGB

--- src/ar/edu/unlu/poo/burako/vista/ColorRGB.java
+++ src/ar/edu/unlu/poo/burako/vista/ColorRGB.java
@@ -4,11 +4,11 @@ import java.awt.*;
 
 public class ColorRGB {
 
+    public static final Color CYAN = new Color(86, 182, 194);
     public static final Color RED = new Color(224, 108, 117);
     public static final Color BLUE = new Color(97, 175, 239);
     public static final Color MAGENTA = new Color(198, 120, 221);
     public static final Color YELLOW = new Color(229, 192, 123);
-    public static final Color CYAN = new Color(86, 182, 194);
     public static final Color GREEN = new Color(152, 195, 121);
     public static final Color ORANGE = new Color(209, 154, 102);
     public static final Color PINK = new Color(152, 195, 121);
