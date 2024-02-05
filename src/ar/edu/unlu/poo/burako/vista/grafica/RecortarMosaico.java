4f982d729dcd9a8c253865bc47e96bdfb9dae9ed
germanritacco <germanritacco@gmail.com>
2024-02-04 23:42:33 -0300

    implementado cache de fichas recortadas

--- src/ar/edu/unlu/poo/burako/vista/grafica/RecortarMosaico.java
+++ src/ar/edu/unlu/poo/burako/vista/grafica/RecortarMosaico.java
@@ -8,15 +8,18 @@ import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RecortarMosaico implements Serializable {
 
     private final BufferedImage originalTexture;
-
+    private final Map<String, ImageIcon> cache;
     private static final int TILE_SIZE = 100;
 
     public RecortarMosaico() {
         originalTexture = loadImage("/ar/edu/unlu/poo/burako/texture/burakoTexture.png"); // Cargar la imagen original
+        cache = new HashMap<>(); // Inicializar el caché
     }
 
     private BufferedImage loadImage(String path) {
@@ -28,36 +31,45 @@ public class RecortarMosaico implements Serializable {
         }
     }
 
-    // Método para obtener la imagen recortada
-    public BufferedImage getImagenRecortada(int color, int numeroFicha) {
-        // Recortar una región específica
-        int x = TILE_SIZE * (numeroFicha);  // Coordenada X del inicio de la nueva imagen
-        int y = TILE_SIZE * color;  // Coordenada Y del inicio de la nueva imagen
-        if (originalTexture != null) {
-            return originalTexture.getSubimage(x, y, TILE_SIZE, TILE_SIZE);
-        } else {
-            return null;
-        }
-    }
-
     public ImageIcon getImagenRecortadaIcon(int color, int numeroFicha, int nuevoAncho, int nuevoAlto, double angulo) {
+        String cacheKey = color + "-" + numeroFicha + "-" + nuevoAncho + "-" + nuevoAlto + "-" + angulo;
+        int ancho = 60;
+        int alto = 80;
+        int desplazamientoX = 20;
+        int desplazamientoY = 10;
+
+        // Verificar si la imagen está en el caché
+        if (cache.containsKey(cacheKey)) {
+            return cache.get(cacheKey);
+        }
+
         // Recortar una región específica
         int x = TILE_SIZE * (numeroFicha);  // Coordenada X del inicio de la nueva imagen
         int y = TILE_SIZE * color;  // Coordenada Y del inicio de la nueva imagen
 
         if (originalTexture != null) {
-            BufferedImage subimage = originalTexture.getSubimage(x, y, TILE_SIZE, TILE_SIZE);
+            BufferedImage subimage;
+            if (angulo == 0) {
+                subimage = originalTexture.getSubimage(x + desplazamientoX, y + desplazamientoY, ancho, alto);
+            } else {
+                subimage = originalTexture.getSubimage(x + desplazamientoY, y + desplazamientoY, alto, alto);
+            }
 
             // Rotar la imagen
             AffineTransform at = new AffineTransform();
-            at.rotate(Math.toRadians(angulo), TILE_SIZE / 2.0, TILE_SIZE / 2.0);
+            at.rotate(Math.toRadians(angulo), alto / 2.0, alto / 2.0);
             AffineTransformOp op = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
             subimage = op.filter(subimage, null);
 
             // Ajustar el tamaño de la imagen
             Image imagenRedimensionada = subimage.getScaledInstance(nuevoAncho, nuevoAlto, Image.SCALE_SMOOTH);
             // Crear un ImageIcon directamente con la imagen redimensionada
-            return new ImageIcon(imagenRedimensionada);
+            ImageIcon imagenIcon = new ImageIcon(imagenRedimensionada);
+
+            // Almacenar en caché el ImageIcon
+            cache.put(cacheKey, imagenIcon);
+
+            return imagenIcon;
         } else {
             return null;
         }
